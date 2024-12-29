933282bb097e536c9b131d399d2a3889d055abbc
Suraj Singh <yousurajsingh@gmail.com>
2024-12-29 21:49:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 function stringChop(str, size) {
-  // your code here
-	if(str==null){
-		return []
-	}
-	let arr = []
-	for(let i=0;i<str.length;i=i+size){
-		 arr.push(str.slice(i,i+size))
-	}
-
-	return arr;
+    let arr = [];
+    let j = size;
+    let i = 0;
+    let s = "";
+    
+    if (str == null || str == undefined) {
+        return arr;
+    } else {
+        while (i < str.length) {
+            while (j > 0) {
+                if (i == str.length) {
+                    break;
+                }
+                s += str[i];
+                i++;
+                j--;
+            }
+            arr.push(s);
+            j = size;
+            s = "";
+        }
+    }
+    return arr; // Move return outside of the loop
 }
 
-Do not change the code below
-
+// Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
